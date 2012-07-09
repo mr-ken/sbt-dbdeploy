226176783c9aefb91aed1b1bf909523446cfb788
mr-ken <kfriesen@thoughtworks.com>
2012-07-09 22:08:29 +0100

    plugin includes all properties

--- src/main/scala/com/dbdeploy/DbDeployPlugin.scala
+++ src/main/scala/com/dbdeploy/DbDeployPlugin.scala
@@ -1,64 +1,67 @@
 package com.dbdeploy
 
+import database.DelimiterType
 import sbt._
-import sbt.Keys._
 
 object DbDeployPlugin extends Plugin {
 
+  val dbDeploySetupTask = TaskKey[DbDeploy]("dbdeploy-setup", "Runs dbdeploy")
   val dbDeployTask = TaskKey[Unit]("dbdeploy", "Runs dbdeploy")
 
-  val dbDeployUserId     = SettingKey[String]("dbdeploy-user-id", "Database user ID")
-  val dbDeployPassword   = SettingKey[String]("dbdeploy-password", "Database user ID password")
-  val dbDeployDriver     = SettingKey[String]("dbdeploy-driver", "Database driver")
-  val dbDeployUrl        = SettingKey[String]("dbdeploy-url", "Database URL")
-  val dbDeployDir        = SettingKey[File]("dbdeploy-dir", "Your script folder")
-  val dbDeployOutputFile = SettingKey[Option[File]]("dbdeploy-output-file", "Output script path + name")
-  val dbDeployDbms       = SettingKey[Option[String]]("dbdeploy-dbms", "Your DBMS")
-  val dbDeployTemplateDir = SettingKey[Option[File]]("dbdeploy-template-dir", "directory for DBMS template scripts, if not using built-in")
-  val dbDeployEncoding    = SettingKey[Option[String]]("dbdeploy-encoding", "charset of SQL scripts - default UTF-8")
-  val dbDeployLastChangeToApply = SettingKey[Option[String]]("dbdeploy-last-change-to-apply", "number of the last script to apply")
-//    val undoOutputFile = SettingKey[String]("dbdeploy-undo-output-file", "undo script path + name")
+  val dbDeployUserId            = SettingKey[String]("dbdeploy-user-id", "Database user ID")
+  val dbDeployPassword          = SettingKey[Option[String]]("dbdeploy-password", "Database user ID password")
+  val dbDeployDriver            = SettingKey[String]("dbdeploy-driver", "Database driver")
+  val dbDeployUrl               = SettingKey[String]("dbdeploy-url", "Database URL")
+  val dbDeployDir               = SettingKey[File]("dbdeploy-dir", "Your script folder")
+  val dbDeployOutputFile        = SettingKey[Option[File]]("dbdeploy-output-file", "Output script path + name")
+  val dbDeployDbms              = SettingKey[Option[String]]("dbdeploy-dbms", "Your DBMS")
+  val dbDeployTemplateDir       = SettingKey[Option[File]]("dbdeploy-template-dir", "Directory for DBMS template scripts, if not using built-in")
+  val dbDeployEncoding          = SettingKey[Option[String]]("dbdeploy-encoding", "Charset of SQL scripts - default UTF-8")
+  val dbDeployLastChangeToApply = SettingKey[Option[Long]]("Dbdeploy-last-change-to-apply", "Number of the last script to apply")
+  val dbDeployUndoOutputFile    = SettingKey[Option[File]]("dbdeploy-undo-output-file", "Undo script path + name")
+  val dbDeployChangeLogTableName= SettingKey[Option[String]]("dbdeploy-changelog-table-name", "Change log table name")
+  val dbDeployDelimiter         = SettingKey[Option[String]]("dbdeploy-delimiter", "Statement delimiter - default ;")
+  val dbDeployDelimiterType     = SettingKey[Option[DelimiterType]]("dbdeploy-delimiter-type", "Statement delimiter type - row or normal, default normal")
 
   lazy val dbDeploySettings = Seq[Setting[_]](
+
     dbDeployDir := file("src/main/dbdeploy"),
+
+    dbDeploySetupTask <<= (dbDeployUserId, dbDeployPassword, dbDeployDriver, dbDeployUrl, dbDeployDir) map {
+      (userId: String, password: Option[String], driver: String, url: String, dir: File) =>
+
+        val dbDeploy = new DbDeploy()
+        dbDeploy.setUserid(userId)
+        password.foreach(dbDeploy.setPassword(_))
+        dbDeploy.setDriver(driver)
+        dbDeploy.setUrl(url)
+        dbDeploy.setScriptdirectory(dir)
+        dbDeploy
+    },
+
     dbDeployOutputFile := None,
     dbDeployDbms := None,
     dbDeployTemplateDir := None,
+    dbDeployLastChangeToApply := None,
+    dbDeployUndoOutputFile := None,
+    dbDeployChangeLogTableName := None,
+    dbDeployDelimiter := None,
+    dbDeployDelimiterType := None,
 
-    dbDeployTask <<= (
-      dbDeployUserId,
-      dbDeployPassword,
-      dbDeployDriver,
-      dbDeployUrl,
-      dbDeployDir,
-      dbDeployOutputFile,
-      dbDeployDbms,
-      dbDeployTemplateDir,
-      dbDeployLastChangeToApply
-    ) map runDbDeploy
+    dbDeployTask <<= (dbDeploySetupTask, dbDeployOutputFile, dbDeployDbms, dbDeployTemplateDir, dbDeployLastChangeToApply, dbDeployUndoOutputFile, dbDeployChangeLogTableName, dbDeployDelimiter, dbDeployDelimiterType) map {
+      (dbDeploy: DbDeploy, outputFile: Option[File], dbms: Option[String], templateDir: Option[File], lastChangeToApply: Option[Long], undoOutputFile: Option[File], changeLogTableName: Option[String], delimiter: Option[String], delimiterType: Option[DelimiterType]) =>
+
+        outputFile.foreach(dbDeploy.setOutputfile(_))
+        dbms.foreach(dbDeploy.setDbms(_))
+        templateDir.foreach(dbDeploy.setTemplatedir(_))
+        lastChangeToApply.foreach(dbDeploy.setLastChangeToApply(_))
+        undoOutputFile.foreach(dbDeploy.setUndoOutputfile(_))
+        changeLogTableName.foreach(dbDeploy.setChangeLogTableName(_))
+        delimiter.foreach(dbDeploy.setDelimiter(_))
+        delimiterType.foreach(dbDeploy.setDelimiterType(_))
+
+        dbDeploy.go()
+    }
   )
 
-  private def runDbDeploy(userId: String,
-                          password: String,
-                          driver: String,
-                          url: String,
-                          dir: File,
-                          outputFile: Option[File],
-                          dbms: Option[String],
-                          templateDir: Option[File],
-                          lastChangeToApply: Option[String]) {
-
-    val dbDeploy = new DbDeploy()
-    dbDeploy.setUserid(userId)
-    dbDeploy.setPassword(password)
-    dbDeploy.setDriver(driver)
-    dbDeploy.setUrl(url)
-    dbDeploy.setScriptdirectory(dir)
-
-    outputFile.foreach(dbDeploy.setOutputfile(_))
-    dbms.foreach(dbDeploy.setDbms(_))
-    templateDir.foreach(dbDeploy.setTemplatedir(_))
-
-    dbDeploy.go()
-  }
 }
