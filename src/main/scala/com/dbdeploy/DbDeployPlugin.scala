891b9f649a54e7d9d2905054c562e59ac5ba1e37
mr-ken <kfriesen@thoughtworks.com>
2012-07-09 11:25:22 +0100

    Refactored a bit

--- src/main/scala/com/dbdeploy/DbDeployPlugin.scala
+++ src/main/scala/com/dbdeploy/DbDeployPlugin.scala
@@ -13,28 +13,52 @@ object DbDeployPlugin extends Plugin {
   val dbDeployUrl        = SettingKey[String]("dbdeploy-url", "Database URL")
   val dbDeployDir        = SettingKey[File]("dbdeploy-dir", "Your script folder")
   val dbDeployOutputFile = SettingKey[Option[File]]("dbdeploy-output-file", "Output script path + name")
-//    val dbms = SettingKey[String]("dbdeploy-dbms", "your DBMS")
-//    val templateDir = SettingKey[String]("dbdeploy-template-dir", "directory for DBMS template scripts, if not using built-in")
-//    val encoding = SettingKey[String]("dbdeploy-encoding", "charset of SQL scripts - default UTF-8")
-//    val lastChangeToApply = SettingKey[String]("dbdeploy-last-change-to-apply", "number of the last script to apply")
+  val dbDeployDbms       = SettingKey[Option[String]]("dbdeploy-dbms", "Your DBMS")
+  val dbDeployTemplateDir = SettingKey[Option[File]]("dbdeploy-template-dir", "directory for DBMS template scripts, if not using built-in")
+  val dbDeployEncoding    = SettingKey[Option[String]]("dbdeploy-encoding", "charset of SQL scripts - default UTF-8")
+  val dbDeployLastChangeToApply = SettingKey[Option[String]]("dbdeploy-last-change-to-apply", "number of the last script to apply")
 //    val undoOutputFile = SettingKey[String]("dbdeploy-undo-output-file", "undo script path + name")
 
   lazy val dbDeploySettings = Seq[Setting[_]](
     dbDeployDir := file("src/main/dbdeploy"),
     dbDeployOutputFile := None,
-    dbDeployTask <<= execute
+    dbDeployDbms := None,
+    dbDeployTemplateDir := None,
+
+    dbDeployTask <<= (
+      dbDeployUserId,
+      dbDeployPassword,
+      dbDeployDriver,
+      dbDeployUrl,
+      dbDeployDir,
+      dbDeployOutputFile,
+      dbDeployDbms,
+      dbDeployTemplateDir,
+      dbDeployLastChangeToApply
+    ) map runDbDeploy
   )
 
-  private def execute = (dbDeployDriver, dbDeployUserId, dbDeployPassword, dbDeployUrl, dbDeployDir, dbDeployOutputFile, streams) map {
-    (driver, userId, password, url, dir, outputFile, str) =>
-      val dbDeploy = new DbDeploy()
-      dbDeploy.setUserid(userId)
-      dbDeploy.setPassword(password)
-      dbDeploy.setDriver(driver)
-      dbDeploy.setUrl(url)
-      dbDeploy.setScriptdirectory(dir)
-      outputFile.foreach(dbDeploy.setOutputfile(_))
-      dbDeploy.go()
-  }
+  private def runDbDeploy(userId: String,
+                          password: String,
+                          driver: String,
+                          url: String,
+                          dir: File,
+                          outputFile: Option[File],
+                          dbms: Option[String],
+                          templateDir: Option[File],
+                          lastChangeToApply: Option[String]) {
 
+    val dbDeploy = new DbDeploy()
+    dbDeploy.setUserid(userId)
+    dbDeploy.setPassword(password)
+    dbDeploy.setDriver(driver)
+    dbDeploy.setUrl(url)
+    dbDeploy.setScriptdirectory(dir)
+
+    outputFile.foreach(dbDeploy.setOutputfile(_))
+    dbms.foreach(dbDeploy.setDbms(_))
+    templateDir.foreach(dbDeploy.setTemplatedir(_))
+
+    dbDeploy.go()
+  }
 }
